e4e38fa3ddad9c828ad8412baf336d8e3fcc3c15
Satyasri-Samayamathula <81159466+Satyasri-Samayamathula@users.noreply.github.com>
2021-05-12 17:05:33 +0530

    Add files via upload

---
+++ capitallist.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CapitalModel } from '../capital.model';
+import { CustomerService } from '../services/customer.service';
+
+@Component({
+  selector: 'app-capitallist',
+  templateUrl: './capitallist.component.html',
+  styleUrls: ['./capitallist.component.css']
+})
+export class CapitallistComponent implements OnInit {
+
+  capital : CapitalModel;
+  found : boolean;
+   constructor(private service : CustomerService, private route : Router) { }
+ 
+   ngOnInit(): void {
+     this.route.navigate(['capitallist']);
+   }
+ 
+   getCapitalById() {
+     this.service
+       .findCapitalById()
+       .then((result : CapitalModel)=> {
+         this.capital = result;
+         this.found = true;
+         console.log(this.capital);
+       });
+   }
+}
